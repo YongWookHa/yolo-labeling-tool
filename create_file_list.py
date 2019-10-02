4f2df63a92bff9916021f9a9cba72532b424b59c
YongWook Ha <ywha12@gmail.com>
2019-10-02 17:04:53 +0900

    add: split dataset

--- create_file_list.py
+++ create_file_list.py
@@ -7,6 +7,7 @@ You still have to make 'project_name.data' file and '*.cfg' file.
 Details could be found in https://github.com/ultralytics/yolov3/wiki/Train-Custom-Data
 '''
 
+import random
 from pathlib import Path
 from tkinter.filedialog import askdirectory
 
@@ -38,8 +39,28 @@ if __name__ == "__main__":
             print(str(img.with_suffix('.txt')), 'is not in all_files')
             continue
         to_write.append(img)
-    
-    with open(project_name+'.txt', 'w', encoding='utf8') as f:
-        for fileObject in to_write:
-            f.write(str(fileObject)+'\n')
+
+    if input('Split dataset into [train / test]? (y/n)') in ('yes', 'y'):
+        train_ratio = float(input('Enter train data ratio (0 ~ 1)'))
+        random.shuffle(to_write)
+        train_num = int(len(to_write) * train_ratio)
+        train_data = to_write[:train_num]
+        test_data = to_write[train_num:]
+
+        with open(project_name+'_train.txt', 'w', encoding='utf8') as f:
+            for fileObject in train_data:
+                f.write(str(fileObject)+'\n')
+            print('Train data saved in ', project_name+'_train.txt')
+        with open(project_name+'_test.txt', 'w', encoding='utf8') as f:
+            for fileObject in test_data:
+                f.write(str(fileObject)+'\n')
+            print('Test data saved in ',project_name+'_test.txt')
+
+    else:
+        with open(project_name+'_total.txt', 'w', encoding='utf8') as f:
+            for fileObject in to_write:
+                f.write(str(fileObject)+'\n')
+            print('Total data saved in ',project_name+'_total.txt')
+
+    input("Press any button to exit")
  
