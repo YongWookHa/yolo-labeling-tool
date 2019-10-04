59b690279cc801125aa04b2ee87fcd3c8da7f739
YongWook Ha <ywha12@gmail.com>
2019-10-04 11:06:14 +0900

    create .names and copy test dataset

--- create_file_list.py
+++ create_file_list.py
@@ -8,6 +8,7 @@ Details could be found in https://github.com/ultralytics/yolov3/wiki/Train-Custo
 '''
 
 import random
+import shutil
 from pathlib import Path
 from tkinter.filedialog import askdirectory
 
@@ -21,17 +22,20 @@ if __name__ == "__main__":
                 project_name = cfg[1]
             if cfg[0][:3] == 'key' and cfg[1] is not '':
                 keys.append(cfg[1])
-    with open(project_name+'.names', 'w', encoding='utf8') as f:
+
+    directory = Path(askdirectory())  # search all sub-directories
+
+    with open(directory / (project_name+'.names'), 'w', encoding='utf8') as f:
         for key in keys:
             f.write(key+'\n')
+        print('.names file save in ', directory / (project_name+'.names'))
     
-    directory = askdirectory()  # search all sub-directories
     images = []
-    for fileObject in Path(directory).glob('**/*'):
+    for fileObject in directory.glob('**/*'):
         if fileObject.suffix in ('.jpg', '.png'):
             images.append(fileObject)
     
-    all_files = list(Path(directory).glob('**/*'))
+    all_files = list(directory.glob('**/*'))
     
     to_write = []
     for img in images:
@@ -40,27 +44,30 @@ if __name__ == "__main__":
             continue
         to_write.append(img)
 
-    if input('Split dataset into [train / test]? (y/n)') in ('yes', 'y'):
-        train_ratio = float(input('Enter train data ratio (0 ~ 1)'))
+    if input('Split dataset into [train / test]? (y/n) : ') in ('yes', 'y'):
+        train_ratio = float(input('Enter train data ratio (0 ~ 1) : '))
         random.shuffle(to_write)
         train_num = int(len(to_write) * train_ratio)
         train_data = to_write[:train_num]
         test_data = to_write[train_num:]
 
-        with open(project_name+'_train.txt', 'w', encoding='utf8') as f:
+        with open(directory / (project_name+'_train.txt'), 'w', encoding='utf8') as f:
             for fileObject in train_data:
                 f.write(str(fileObject)+'\n')
-            print('Train data saved in ', project_name+'_train.txt')
-        with open(project_name+'_test.txt', 'w', encoding='utf8') as f:
+            print('Train data list saved in ', directory / (project_name+'_train.txt'))
+        with open(directory / (project_name+'_test.txt'), 'w', encoding='utf8') as f:
+            (directory / 'test_data').mkdir(exist_ok=True)
             for fileObject in test_data:
                 f.write(str(fileObject)+'\n')
-            print('Test data saved in ',project_name+'_test.txt')
+                shutil.copyfile(str(fileObject), str((directory / 'test_data' / fileObject.name)))
+            print('Test data list saved in ', directory / (project_name+'_test.txt'))
+            print('Test data copied at ', directory / 'test')
 
     else:
-        with open(project_name+'_total.txt', 'w', encoding='utf8') as f:
+        with open(directory / (project_name+'_total.txt'), 'w', encoding='utf8') as f:
             for fileObject in to_write:
                 f.write(str(fileObject)+'\n')
-            print('Total data saved in ',project_name+'_total.txt')
+            print('Total data saved in ', directory / (project_name+'_total.txt'))
 
-    input("Press any button to exit")
+    input("\nPress any button to exit")
  
